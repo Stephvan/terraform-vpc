b7ad839dbfe19290c12773cd92b91537e186685b
stephen oyetoro <stephen.oyetoro@mantleanalyticsas.onmicrosoft.com>
2023-07-23 06:48:09 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ variable "networking" {
   })
   default = {
     cidr_block      = "10.0.0.0/16"
-    region          = "us-east-1"
+    region          = "us-west-2"
     vpc_name        = "custom-vpc"
-    azs             = ["us-east-1a", "us-east-1b"]
+    azs             = ["us-west-1a", "us-west-1b"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
